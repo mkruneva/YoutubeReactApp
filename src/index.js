45eaf0958e5b5ee77868dd050a777fdbc6ee5020
mkruneva <marta.kruneva@gmail.com>
2018-05-26 20:56:49 +0100

    components rename, SearchBar

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 
 import API_KEY from './keys';
 
+import SearchBar from './components/search-bar'
+
 // Create a new component 
 // Component to produce HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
-
-// const App = function() {
-//     return <div>Hi!</div>;
-// }
 
 // Take generated HTML and add to DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
