4c7dd0f6549ef00fb479735b775ca24e8c7ee295
mkruneva <marta.kruneva@gmail.com>
2018-06-06 11:13:13 +0000

    list items markup

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+    // const video = props.video; // used {video} instead
+    const imgUrl = video.snippet.thumbnails.default.url;
+    console.log(imgUrl);
     return (
-        <li>{props.video.etag}</li>
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imgUrl}/>
+                </div>
+
+                <div className="media-body">
+                    <div className="media-hedaing">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
     );
 }
 
